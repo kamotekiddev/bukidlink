7de105cffbcf7a7812fb72a261f7f6fac7914b88
joshua.dev.ph <joshua.dev.ph@tripleonetech.net>
2025-06-05 15:41:36 +0800

    setup: setup shadcn ui
    setup: setup shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Button } from '@/components/ui/button';
 
 function Home() {
-    return <div className="p-4">This is the page</div>;
+    return (
+        <div className="p-4">
+            <Button>Button</Button>
+        </div>
+    );
 }
 
 export default Home;
